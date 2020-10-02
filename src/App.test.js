83fb372cdfcb9ed407b468c78e50cbea479f1065
Brian Kubes <briankubes@gmail.com>
2020-10-01 17:46:57 -0700

    working but I want to remove the onChange

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, findByText } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
+test("renders and inputs work", () => {
   render(<App />);
+
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const messageInput = screen.getByLabelText(/message/i);
+  const submitButton = screen.getByRole("button", /submit/i);
+
+  fireEvent.change(firstNameInput, { target: { value: "Brian" } });
+  fireEvent.change(lastNameInput, {
+    target: { name: "lastName", value: "Kubes" },
+  });
+  fireEvent.change(emailInput, { target: { value: "me@me.com" } });
+
+  fireEvent.click(submitButton);
+
+  const checkName = screen.findByText(/brian/i);
 });
